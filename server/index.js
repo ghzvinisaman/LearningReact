fd9dd6957c894292392fd6f78e78f79252a3bc57
MahtaMR <zohreh.torabian7366@gmail.com>
2017-04-20 17:40:40 +0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import path from 'path';
+const app = express();
+
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname,'../index.html'));
+});
+
+app.listen(8080,()=>console.log('Runnig serer on localhost:8080'));
